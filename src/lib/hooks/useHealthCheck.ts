7d7cef32e5d559ed1a2fcfca35b90fe287f1efda
kuattovak <108578467+kuattovak@users.noreply.github.com>
2025-02-20 17:01:04 +0500

    Update useHealthCheck.ts

--- src/lib/hooks/useHealthCheck.ts
+++ src/lib/hooks/useHealthCheck.ts
@@ -199,7 +199,7 @@ export const useHealthCheck = (): HealthCheckState & {
         const toastId = toast.loading("Отправка данных...");
 
         try {
-            const response = await fetch("http://localhost:3001", {
+            const response = await fetch("http://localhost:3001/health", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(finalData),
